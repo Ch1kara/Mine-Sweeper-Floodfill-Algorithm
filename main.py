008f52d0dae593a915f961a38dfd70658d09fe12
Ch1kara <andrew.tv.tieu@gmail.com>
2024-01-14 15:01:36 -0800

    idk

--- main.py
+++ main.py
@@ -8,8 +8,3 @@ screensize = (900, 900)
 
 game = Interface(grid, screensize)
 game.gameloop()
-
-# Notes for tomorrow:
-# - create a home menu before starting game with size control and exit features
-#  easy: 9x9, medium: 16x16, hard: 20x20
-#  stop click registering after generating from menu screen
